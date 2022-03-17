03d199d36169ac6ee4880f4ab2448cbe3536f628
Getúlio Souza <getuliosouza@live.com>
2022-03-17 14:28:57 -0300

    layout original com javascript inativo

--- pages/app.js
+++ pages/app.js
@@ -1,5 +1,8 @@
+// esconder barra de navegação após o scroll
 window.addEventListener("scroll", ()=>{
     const menuNav = document.querySelector(".menu");
     menuNav.classList.toggle("scroll-active",
     window.scrollY> 80)
 });
+
+// image silder
