6f2c176be5d48bdf1a93cfc71fe8d6b8fde73fcb
Getúlio Souza <getuliosouza@live.com>
2022-03-17 19:51:10 -0300

    galeria original

--- pages/app.js
+++ pages/app.js
@@ -6,3 +6,31 @@ window.addEventListener("scroll", ()=>{
 });
 
 // image silder
+
+const buttons = document.querySelectorAll("[data-carrosel]");
+
+buttons.forEach(button =>{
+    button.addEventListener("click", ()=>{
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if(newIndex < 0) slides.children.length -1
+        if(newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+///https://www.varonis.com/blog/git-branching
